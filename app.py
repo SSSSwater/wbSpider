64010fca931c1895fde06a471e7034d66ca3d26c
mizuss <307722647@qq.com>
2025-04-27 19:04:23 +0800

    aa

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
 def anl():
@@ -12,6 +14,25 @@ def intro():
 def index():
    return render_template('index.html')
 
+parser = reqparse.RequestParser()
+parser.add_argument('userId', type=int, help='Rate to charge for this resource')
+
+class UserInfo(Resource):
+    def get(self):
+        args = parser.parse_args()
+        print(args)
+
+
+api.add_resource(UserInfo, '/anl/user')
+# @app.route('/anl/user', methods=["GET","POST"])
+# def anl_user():
+#     json_value = request.get_json()
+#     print(json_value['input'])
+# # 发送数据
+#     info = dict()
+#     info["status"] = "success"
+#     info["page"] = "/test/lyshark"
+#     return jsonify(info)
 
 if __name__ == '__main__':
     app.run()
